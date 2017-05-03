5adb022afa7acaf7d0feede72b83af26aa9bc552
Ivan <hubbu09@gmail.com>
2017-05-03 12:34:04 -0400

    Stub out title screen

---
+++ sfml-pong/source/TitleScreen.cpp
@@ -0,0 +1,25 @@
+#include "TitleScreen.h"
+#include <SFML/Window/Event.hpp>
+
+namespace Pong
+{
+	void TitleScreen::draw(sf::RenderTarget* target)
+	{
+	}
+
+	void TitleScreen::on_start()
+	{
+	}
+
+	void TitleScreen::update(float dt)
+	{
+	}
+
+	void TitleScreen::handle_input(sf::Event event)
+	{
+	}
+
+	void TitleScreen::on_stop()
+	{
+	}
+}
--- sfml-pong/source/main.cpp
+++ sfml-pong/source/main.cpp
@@ -2,13 +2,14 @@
 
 #include "Window.h"
 #include "GameScreen.h"
+#include "TitleScreen.h"
 
 int main() 
 {
 	try
 	{
-		Pong::GameScreen game_screen;
-		Pong::Window window { "Pong",  &game_screen };
+		Pong::TitleScreen landing_screen;
+		Pong::Window window { "Pong",  &landing_screen };
 	} 
 	catch (std::exception& e)
 	{
