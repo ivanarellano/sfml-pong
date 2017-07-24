a7f36355a6daf34a45605b62a0c7e839b0123126
Ivan <ia@ivanarellano.com>
2017-07-24 14:03:44 -0400

    Minor housekeeping

--- sfml-pong/include/View/HalfCourtLine.h
+++ sfml-pong/include/View/HalfCourtLine.h
@@ -6,14 +6,14 @@ namespace Pong
 {
 	/*
 	* A half court line with a dashed line effect.
-	* The width and height set the size of a single dash.
+	* The width and height represent the size of a single dash.
 	*/
 	class HalfCourtLine : public Drawable
 	{
 	public:
 		HalfCourtLine();
 
-		explicit HalfCourtLine(sf::Vector2f size, float gap);
+		HalfCourtLine(sf::Vector2f size, float gap);
 
 		void draw(sf::RenderTarget* target) override;
 	private:
